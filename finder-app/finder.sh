f475a1c46c8e98985e97a2f424890108f75b31bf
rounaksaha <saharounak93@gmail.com>
2024-09-21 18:27:55 -0700

    Assignment 1 Part 9

---
+++ finder-app/finder.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+filesdir=$1
+searchstr=$2
+echo "filesdir: ${filesdir}, searchstr: ${searchstr}"
+if [ $# -eq 2 ] 
+then 
+	echo "2 Args provided as expected! "
+	if [ ! -d ${filesdir} ]
+	then
+		echo "Error: Directory ${filesdir} does not exist! "
+		exit 1
+	else
+		filecount=$(find ${filesdir} | wc -l)
+		linecount=$(grep -o -r ${searchstr} ${filesdir} | wc -l)
+		echo "The number of files are ${filecount} and the number of matching lines are ${linecount}" 
+	fi
+	exit 0
+else
+	echo "Error: Invalid Args, Please provide 2 args from the cmd line! "
+	exit 1
+fi
+
