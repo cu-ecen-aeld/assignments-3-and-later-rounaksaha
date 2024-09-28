34a3ca1086d03e5d89a94bcabf783edc83dea113
rounaksaha <saharounak93@gmail.com>
2024-09-27 23:30:49 -0700

    Upload Assignment 2 till Part 4 Makefile

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -5,6 +5,9 @@
 
 int main()
 {
+	//LeoDebug
+	printf("writer.c has been called \n");
+
 	openlog("LeoLog1", LOG_PID | LOG_CONS, LOG_USER);
 	syslog(LOG_DEBUG, "Program Started!"); 
 
@@ -21,15 +24,17 @@ int main()
 
 	if(fptr==NULL)
 	{
-		syslog(LOG_ERR, "Error: Failed to create file: %s \n", strerror(errno));
+		syslog(LOG_ERR, "Error: Failed to create file! \n");
+		printf("file could not be created \n");
 		return 1;
 	}
 
 	//Write to the File
-	fprintf(fptr, writestr);
+	fprintf(fptr, "%s", writestr);
+	printf("string written to file \n");
 	//Close the File
 	fclose(fptr);
-
+	printf("file close \n");
 	closelog();
 	return 0;
 }
